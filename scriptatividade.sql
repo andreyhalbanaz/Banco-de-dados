d9c67c9eaf37f7eb354164807358ad4a0c42f11f
andreyhalbanaz <andreyalbanaz@gmail.com>
2026-06-10 15:49:26 -0300

    atividade

---
+++ scriptatividade.sql
@@ -0,0 +1,62 @@
+-- Atividade 1
+USE HR;
+SELECT * FROM employees; 
+
+-- Atividade 2
+SELECT department_id, department_name, location_id
+FROM departments;
+
+-- Atividade 3 
+SELECT job_id, job_title, max_salary
+FROM jobs;
+
+-- Atividade 4 
+SELECT job_title, max_salary, job_id
+FROM jobs;
+
+-- Atividade 5 
+SELECT first_name, last_name, email, phone_number
+FROM employees;
+
+-- Atividade 6
+SELECT last_name, salary, 500 + salary
+FROM employees;
+
+-- Atividade 7
+SELECT last_name, salary, salary * 12
+FROM employees;
+
+-- Atividade 8
+-- Porque os numeros e operadores entre parenteses tem prioridade, então na opção B somaria primeiro o (12+1000) e depois multiplicaria.
+
+-- Atividade 9 
+SELECT last_name, job_id, salary, commission_pct, salary * 12 * commission_pct
+FROM employees; 
+
+-- Atividade 10
+SELECT last_name AS 'Sobrenome', salary AS 'Salario Mensal', salary * 12 AS 'Salario Anual'
+FROM employees;
+
+-- Atividade 11
+SELECT CONCAT(first_name, ' ', last_name, '( Email: ', email, ')') AS 'Identificação'
+FROM employees;
+
+-- Atividade 12 
+SELECT concat(last_name, ' trabalha no cargo ', job_id, ' e ganha ', salary)
+FROM employees;
+
+-- Atividade 13 
+SELECT concat(job_title, ': ', ' de ',  min_salary, ' até ',  max_salary) AS 'Faixa Salarial'
+FROM jobs;
+
+
+
+
+
+
+
+
+
+
+
+
